fac98a6cc5d3f3616b178ae7097750d5b3b78e79
David Andrade <david.andrade7477@alumnos.udg.mx>
2020-10-04 23:08:34 -0500

    Implementadas las indicaciones del programa

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
-
+    string cadena;
+    getline (cin,cadena);
+    if (cadena == "char")
+        cout << sizeof(char) << endl;
+    else if (cadena == "int")
+        cout << sizeof(int) << endl;
+    else if (cadena == "short")
+        cout << sizeof(short) << endl;
+    else if (cadena == "float")
+        cout << sizeof(float) << endl;
+    else if (cadena == "double")
+        cout << sizeof(double) << endl;
+    else if (cadena == "long")
+        cout << sizeof(long) << endl;
     return 0;
 }
